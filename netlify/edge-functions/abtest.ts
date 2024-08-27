eca09f07ac7491d06fd52c4822367565dfed31c6
Rajshekhar Reddy <126725003+rrajshekhar@users.noreply.github.com>
2024-08-27 15:44:13 -0400

    Leverage env variable

--- netlify/edge-functions/abtest.ts
+++ netlify/edge-functions/abtest.ts
@@ -1,20 +1,20 @@
 import type { Context, Config } from "@netlify/edge-functions";
-const BUCKET_COOKIE_NAME = "test_bucket";
+const BUCKET_COOKIE_NAME = "force_redirect";
 const REDIRECT_URL = "https://www-silversea.uat.bbhosted.com";
-const BUCKET_WEIGHTING = 0.5;
+const BUCKET_WEIGHTING = Netlify.env.get("BUCKET_WEIGHTING");
 export default async (request: Request, context: Context) => {
   const existingBucket = context.cookies.get(BUCKET_COOKIE_NAME);
   if (existingBucket) {
-    return existingBucket === 'b'
+    return existingBucket === 'bb'
       ? Response.redirect(REDIRECT_URL, 301)
       : context.next();
   }
-  const newBucket = Math.random() <= BUCKET_WEIGHTING ? "a" : "b";
+  const newBucket = Math.random() <= BUCKET_WEIGHTING ? "ssc" : "bb";
   context.cookies.set({
     name: BUCKET_COOKIE_NAME,
     value: newBucket,
   });
-  return newBucket === 'b'
+  return newBucket === 'bb'
     ? Response.redirect(REDIRECT_URL, 301)
     : context.next();
 };
