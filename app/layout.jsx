5aba224dc8874fa7c27d28c3d24a0426162dd4d2
Rajshekhar Reddy <126725003+rrajshekhar@users.noreply.github.com>
2024-08-29 21:51:30 -0400

    add script

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
+import Script from 'next/script'
 
 export const metadata = {
     title: {
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
         <html lang="en" data-theme="lofi">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
+                <Script src="google-analytics.js" strategy="afterInteractive"/>
             </head>
             <body className="antialiased text-white bg-blue-900">
                 <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
