2d2948fcc17549bd2190a4a24197efc9b6d45cd2
Rajshekhar Reddy <rrami@rccl.com>
2024-08-30 20:34:13 -0400

    Refactoring

--- netlify/edge-functions/abtest.ts
+++ netlify/edge-functions/abtest.ts
@@ -1,35 +1,45 @@
 import type { Context, Config } from "@netlify/edge-functions";
 
-const BUCKET_COOKIE_NAME = "edge_redirect";
-const REDIRECT_URL = "https://www-silversea.uat.bbhosted.com/";
-const BUCKET_WEIGHTING = Netlify.env.get("BUCKET_WEIGHTING");
+const REDIRECT_COOKIE = "edge_redirect";
+const TRANSCODING_URL = Netlify.env.get("TRANSCODING_URL") || "/";
+const TRANSCODING_TRAFFIC_PERCENTAGE = Netlify.env.get("TRANSCODING_TRAFFIC_PERCENTAGE");
+
 export default async (request: Request, context: Context) => {
+
   const url = new URL(request.url);
   const path = url.pathname;
-  const redirectUrl = new URL(path, REDIRECT_URL).toString();
-  
+  const redirectUrl = new URL(path, TRANSCODING_URL).toString();
   const forceOverride = url.searchParams.get("forceOverride");
+
   if(forceOverride === 'bb') {
      return Response.redirect(redirectUrl, 301)
   }
+
   if(forceOverride === 'ssc') {
      return context.next();
   }
-  const existingBucket = context.cookies.get(BUCKET_COOKIE_NAME);
-  if (existingBucket) {
-    return existingBucket === 'bb'
-      ? Response.redirect(redirectUrl, 301)
-      : context.next();
+
+  const redirectCookie = context.cookies.get(REDIRECT_COOKIE);
+
+  if(redirectCookie) {
+      return redirect(redirectCookie, redirectUrl, context);
   }
-  const newBucket = Math.random() <= BUCKET_WEIGHTING ? "ssc" : "bb";
+  const isTrancoded = Math.random() <= TRANSCODING_TRAFFIC_PERCENTAGE ? "ssc" : "bb";
+
   context.cookies.set({
-    name: BUCKET_COOKIE_NAME,
-    value: newBucket,
+    name: REDIRECT_COOKIE,
+    value: isTrancoded,
   });
-  return newBucket === 'bb'
+  return redirect(isTrancoded, redirectUrl, context);
+};
+
+function redirect(isTranscoded: string, redirectUrl: string, context: Context) {
+  return isTranscoded === 'bb'
     ? Response.redirect(redirectUrl, 301)
     : context.next();
-};
+}
+
 export const config: Config = {
   path: "/*",
 };
+
