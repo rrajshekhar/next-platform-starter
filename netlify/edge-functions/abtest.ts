d7567cb3aaf1507300305c35d198499628a12f67
Rajshekhar Reddy <rrami@rccl.com>
2024-09-03 15:52:45 -0400

    add logging

--- netlify/edge-functions/abtest.ts
+++ netlify/edge-functions/abtest.ts
@@ -9,13 +9,12 @@ export default async (request: Request, context: Context) => {
 
   const url = new URL(request.url);
   const path = url.pathname;
-
-  console.log('path is',path);
-  
+ 
    const forceOverride = url.searchParams.get("forceOverride");
    const redirectCookie = context.cookies.get(REDIRECT_COOKIE);
 
   if(TRANSCODING_URL === undefined || forceOverride === 'ssc' || path.startsWith('de')|| path.startsWith('pt-br') || path.startsWith('es') || path.startsWith('fr')) {
+    console.log('entered path logic', path)
     if(redirectCookie){
       context.cookies.set({
         name: REDIRECT_COOKIE,
