e4dae6e1986478f470ee8eb402c4194de913ae31
Rajshekhar Reddy <rrami@rccl.com>
2024-09-09 13:07:56 -0400

    triage the edge

--- netlify/edge-functions/abtest.ts
+++ netlify/edge-functions/abtest.ts
@@ -8,49 +8,39 @@ const UNSUPPORTED_LANGUAGES = ['/de','/pt-br','/es','/fr'];
 export default async (request: Request, context: Context) => {
 
 
-//   const url = new URL(request.url);
-//   const path = url.pathname;
+ const url = new URL(request.url);
+ const path = url.pathname;
 
-//   const forceOverride = url.searchParams.get("forceOverride");
-//   const proxyCookie = context.cookies.get(PROXY_COOKIE);
+  const forceOverride = url.searchParams.get("forceOverride");
+  const proxyCookie = context.cookies.get(PROXY_COOKIE);
 
-//    // Sample for expiry
-//    const now = new Date();
-//    const time = now.getTime();
-//    const expireTime = time + 1000*36000;
+    // Sample for expiry
+   const now = new Date();
+   const time = now.getTime();
+   const expireTime = time + 1000*36000;
 
-//   if(TRANSCODING_URL === undefined || validateLanguage(path) || forceOverride === 'ssc') {
-//     if(proxyCookie){
-//       context.cookies.set({
-//         name: PROXY_COOKIE,
-//         value: "ssc",
-//         expires: expireTime
-//       });
-//     }
-//     return;
-//  }
+  if(TRANSCODING_URL === undefined || validateLanguage(path) || forceOverride === 'ssc') {
+    if(proxyCookie){
+      context.cookies.set({
+        name: PROXY_COOKIE,
+        value: "ssc",
+        expires: expireTime
+      });
+    }
+    return context.next();
+ }
 
 //   const proxyUrl =new URL(path, TRANSCODING_URL).toString();
+  if(proxyCookie) {
+      return context.next();
+  }
+   const trafficRouting = Math.random() <= TRANSCODING_TRAFFIC_PERCENTAGE ? "ssc" : "bb";
 
-//   if(forceOverride === 'bb') {
-//     context.cookies.set({
-//       name: PROXY_COOKIE,
-//       value: "bb",
-//       expires: expireTime
-//     });
-//     return context.next();
-//   }
-
-//   if(proxyCookie) {
-//       return context.next();
-//   }
-//   const trafficRouting = Math.random() <= TRANSCODING_TRAFFIC_PERCENTAGE ? "ssc" : "bb";
-
-//   context.cookies.set({
-//     name: PROXY_COOKIE,
-//     value: trafficRouting,
-//     expires: expireTime
-//   });
+  context.cookies.set({
+    name: PROXY_COOKIE,
+    value: trafficRouting,
+    expires: expireTime
+  });
 
 //   return context.next();
 // };
@@ -61,9 +51,9 @@ export default async (request: Request, context: Context) => {
  
 // //}
 
-// function validateLanguage(path) {
-//   return UNSUPPORTED_LANGUAGES.some(languages => path.startsWith(languages))
-// }
+ function validateLanguage(path) {
+   return UNSUPPORTED_LANGUAGES.some(languages => path.startsWith(languages))
+}
 
 }
 
