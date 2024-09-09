e0f8d6156381dc8d40e0368654616891daa705e2
Rajshekhar Reddy <rrami@rccl.com>
2024-09-09 11:36:13 -0400

    revert

--- netlify/edge-functions/abtest.ts
+++ netlify/edge-functions/abtest.ts
@@ -54,7 +54,7 @@ export default async (request: Request, context: Context) => {
 
 async function redirect(isTranscoded: string, redirectUrl: string, context: Context) {
   const headers = {
-    'Content-Type' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'
+    'Content-Type' : 'text/html'
   };
 
   return isTranscoded === 'bb' ? await fetch(redirectUrl, {
