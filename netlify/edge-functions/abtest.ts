f78dced739285c3abb24d8305a3f31774c4cb67c
Rajshekhar Reddy <rrami@rccl.com>
2024-09-09 12:45:28 -0400

    troubleshooot

--- netlify/edge-functions/abtest.ts
+++ netlify/edge-functions/abtest.ts
@@ -8,61 +8,63 @@ const UNSUPPORTED_LANGUAGES = ['/de','/pt-br','/es','/fr'];
 export default async (request: Request, context: Context) => {
 
 
-  const url = new URL(request.url);
-  const path = url.pathname;
+//   const url = new URL(request.url);
+//   const path = url.pathname;
 
-  const forceOverride = url.searchParams.get("forceOverride");
-  const proxyCookie = context.cookies.get(PROXY_COOKIE);
+//   const forceOverride = url.searchParams.get("forceOverride");
+//   const proxyCookie = context.cookies.get(PROXY_COOKIE);
 
-   // Sample for expiry
-   const now = new Date();
-   const time = now.getTime();
-   const expireTime = time + 1000*36000;
+//    // Sample for expiry
+//    const now = new Date();
+//    const time = now.getTime();
+//    const expireTime = time + 1000*36000;
 
-  if(TRANSCODING_URL === undefined || validateLanguage(path) || forceOverride === 'ssc') {
-    if(proxyCookie){
-      context.cookies.set({
-        name: PROXY_COOKIE,
-        value: "ssc",
-        expires: expireTime
-      });
-    }
-    return context.next();
- }
+//   if(TRANSCODING_URL === undefined || validateLanguage(path) || forceOverride === 'ssc') {
+//     if(proxyCookie){
+//       context.cookies.set({
+//         name: PROXY_COOKIE,
+//         value: "ssc",
+//         expires: expireTime
+//       });
+//     }
+//     return;
+//  }
 
-  const proxyUrl =new URL(path, TRANSCODING_URL).toString();
+//   const proxyUrl =new URL(path, TRANSCODING_URL).toString();
 
-  if(forceOverride === 'bb') {
-    context.cookies.set({
-      name: PROXY_COOKIE,
-      value: "bb",
-      expires: expireTime
-    });
-    return context.next();
-  }
+//   if(forceOverride === 'bb') {
+//     context.cookies.set({
+//       name: PROXY_COOKIE,
+//       value: "bb",
+//       expires: expireTime
+//     });
+//     return context.next();
+//   }
 
-  if(proxyCookie) {
-      return context.next();
-  }
-  const trafficRouting = Math.random() <= TRANSCODING_TRAFFIC_PERCENTAGE ? "ssc" : "bb";
+//   if(proxyCookie) {
+//       return context.next();
+//   }
+//   const trafficRouting = Math.random() <= TRANSCODING_TRAFFIC_PERCENTAGE ? "ssc" : "bb";
 
-  context.cookies.set({
-    name: PROXY_COOKIE,
-    value: trafficRouting,
-    expires: expireTime
-  });
+//   context.cookies.set({
+//     name: PROXY_COOKIE,
+//     value: trafficRouting,
+//     expires: expireTime
+//   });
 
-  return context.next();
-};
+//   return context.next();
+// };
 
-// async function redirect(isTranscoded: string, redirectUrl: string, context: Context) {
-//   return isTranscoded === 'bb' ? 
-//   context.redirect(redirectUrl, 200): context.next();
+// // async function redirect(isTranscoded: string, redirectUrl: string, context: Context) {
+// //   return isTranscoded === 'bb' ? 
+// //   context.redirect(redirectUrl, 200): context.next();
  
-//}
+// //}
 
-function validateLanguage(path) {
-  return UNSUPPORTED_LANGUAGES.some(languages => path.startsWith(languages))
+// function validateLanguage(path) {
+//   return UNSUPPORTED_LANGUAGES.some(languages => path.startsWith(languages))
+// }
+
 }
 
 export const config: Config = {
