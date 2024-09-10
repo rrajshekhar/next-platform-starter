71bf13c9cd9e9e9b5994ea8e53aca78195dfd59b
Rajshekhar Reddy <rrami@rccl.com>
2024-09-10 18:39:55 -0400

    last commit

--- netlify/edge-functions/abtest.ts
+++ netlify/edge-functions/abtest.ts
@@ -1,41 +1,74 @@
 import type { Context, Config } from "@netlify/edge-functions";
+
 const PROXY_COOKIE = "edge_proxy";
-const TRAFFIC_PERCENTAGE = parseFloat(Netlify.env.get("TRANSCODING_TRAFFIC_PERCENTAGE") ?? "0.5");
+const TRANSCODING_URL = Netlify.env.get("TRANSCODING_URL");
+const TRANSCODING_TRAFFIC_PERCENTAGE = parseFloat(Netlify.env.get("TRANSCODING_TRAFFIC_PERCENTAGE") ?? 1);
 const UNSUPPORTED_LANGUAGES = ['/de','/pt-br','/es','/fr'];
+
 export default async (request: Request, context: Context) => {
+
+
   const url = new URL(request.url);
   const path = url.pathname;
-  const now = new Date();
-  const expireTime = now.getTime() + 1000 * 36000;
-  const proxyCookie = context.cookies.get(PROXY_COOKIE);
-  const forceOverride = url.searchParams.get("forceOverride");
 
-  if (validateLanguage(path) || forceOverride === 'ssc') {
-    if (proxyCookie) {
-      context.cookies.delete(PROXY_COOKIE);
+  const forceOverride = url.searchParams.get("forceOverride");
+  const proxyCookie = context.cookies.get(PROXY_COOKIE);
+
+   // Sample for expiry
+   const now = new Date();
+   const time = now.getTime();
+   const expireTime = time + 1000*36000;
+
+  if(TRANSCODING_URL === undefined || validateLanguage(path) || forceOverride === 'ssc') {
+    if(proxyCookie){
+      context.cookies.set({
+        name: PROXY_COOKIE,
+        value: "ssc",
+        expires: expireTime
+      });
     }
-    const response = await context.next({ sendConditionalRequest: true });
-    return response;
+    return context.next();
+ }
+
+  const proxyUrl =new URL(path, TRANSCODING_URL).toString();
+
+  if(forceOverride === 'bb') {
+     return redirect('bb', proxyUrl, context);
   }
-  
-  if(!proxyCookie) {
-  const trafficRouting = forceOverride || (Math.random() <= TRAFFIC_PERCENTAGE ? "ssc" : "bb");
-  if ((trafficRouting === 'bb' || forceOverride === 'bb')) {
-    context.cookies.set({
-      name: PROXY_COOKIE,
-      value: trafficRouting,
-      domain: 'ssc-preview-edge.netlify.app',
-      expires: new Date(expireTime),
-    });
-  }};
-  return;
+
+  if(proxyCookie) {
+      return redirect(proxyCookie, proxyUrl, context);
+  }
+  const trafficRouting = Math.random() <= TRANSCODING_TRAFFIC_PERCENTAGE ? "ssc" : "bb";
+
+ 
+
+
+  context.cookies.set({
+    name: PROXY_COOKIE,
+    value: trafficRouting,
+    expires: expireTime
+  });
+
+  return redirect(trafficRouting, proxyUrl, context);
 };
+
+async function redirect(isTranscoded: string, redirectUrl: string, context: Context) {
+  const headers = {
+    'Content-Type' : 'text/html'
+  };
+
+  return isTranscoded === 'bb' ? await fetch(redirectUrl, {
+    headers: headers,
+  }): context.next();
+ 
+}
 
 function validateLanguage(path) {
   return UNSUPPORTED_LANGUAGES.some(languages => path.startsWith(languages))
 }
 
 export const config: Config = {
-  path: "/*",
-  excludedPath: ["/*.css", "/*.js","/*.svg"]
+  path: "/*"
 };
+
