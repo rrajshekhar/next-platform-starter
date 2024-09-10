8512ca3bc4ed17d8397d6d76d4d4e085c242817a
Rajshekhar Reddy <rrami@rccl.com>
2024-09-09 22:22:25 -0400

    build

--- netlify/edge-functions/abtest.ts
+++ netlify/edge-functions/abtest.ts
@@ -4,13 +4,13 @@ const TRAFFIC_PERCENTAGE = parseFloat(Netlify.env.get("TRAFFIC_PERCENTAGE") ?? "
 export default async (request: Request, context: Context) => {
   const url = new URL(request.url);
   const path = url.pathname;
-  console.log('path is', path);
   const now = new Date();
   const expireTime = now.getTime() + 1000 * 36000;
   const override = url.searchParams.get("forceOverride");
   const currentCookie = context.cookies.get(PROXY_COOKIE);
   // Handle overrides
   if (override) {
+    console.log('path is', path);
     if (override === 'ssc') {
       context.cookies.delete(PROXY_COOKIE);
     } else if (override === 'bb') {
