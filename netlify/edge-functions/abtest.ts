1abb0c37c9014c61f0f56c0b3ce86ba5c3b77222
Rajshekhar Reddy <rrami@rccl.com>
2024-09-09 20:07:34 -0400

    redirect 1

--- netlify/edge-functions/abtest.ts
+++ netlify/edge-functions/abtest.ts
@@ -8,43 +8,43 @@ const UNSUPPORTED_LANGUAGES = ['/de','/pt-br','/es','/fr'];
 export default async (request: Request, context: Context) => {
 
 
-//   const url = new URL(request.url);
-//   const path = url.pathname;
-//   const now = new Date();
-//   const time = now.getTime();
-//   const expireTime = time + 1000*36000;
-//   const pastExpiryTime = time - 1000*36000;
+  const url = new URL(request.url);
+  const path = url.pathname;
+  const now = new Date();
+  const time = now.getTime();
+  const expireTime = time + 1000*36000;
+  const pastExpiryTime = time - 1000*36000;
 
-//   const forceOverride = url.searchParams.get("forceOverride");
-//   const proxyCookie = context.cookies.get(PROXY_COOKIE);
+  const forceOverride = url.searchParams.get("forceOverride");
+  const proxyCookie = context.cookies.get(PROXY_COOKIE);
 
-//   if(forceOverride === 'ssc') {
-//     if(proxyCookie){
-//       context.cookies.set({
-//         name: PROXY_COOKIE,
-//         expiry : pastExpiryTime
-//       });
-//     }
-//     console.log(proxyCookie);
-//     console.log(context.cookies.get(PROXY_COOKIE));
-//     return context.next();
-//  }
+  if(forceOverride === 'ssc') {
+    if(proxyCookie){
+      context.cookies.set({
+        name: PROXY_COOKIE,
+        expiry : pastExpiryTime
+      });
+    }
+    console.log(proxyCookie);
+    console.log(context.cookies.get(PROXY_COOKIE));
+    return context.next();
+ }
 
-//   if(proxyCookie) {
-//       return context.next();
-//   }
+  if(proxyCookie) {
+      return context.next();
+  }
 
-//   const trafficRouting = Math.random() <= TRANSCODING_TRAFFIC_PERCENTAGE ? "ssc" : "bb";
+  const trafficRouting = Math.random() <= TRANSCODING_TRAFFIC_PERCENTAGE ? "ssc" : "bb";
 
 
-//   if(forceOverride === 'bb' || trafficRouting === 'bb') {
-//     context.cookies.set({
-//       name: PROXY_COOKIE,
-//       value: trafficRouting,
-//       expires: expireTime
-//     });
-//   }
-//   return context.next();
+  if(forceOverride === 'bb' || trafficRouting === 'bb') {
+    context.cookies.set({
+      name: PROXY_COOKIE,
+      value: trafficRouting,
+      expires: expireTime
+    });
+  }
+  return context.next();
 };
 
 
@@ -55,6 +55,6 @@ function validateLanguage(path) {
 
 export const config: Config = {
   path: "/*",
-  excludedPath: ["/*static*"]
+  excludedPath: ["/_next*"]
 };
 
